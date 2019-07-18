81766f1128571483e035b9e73acef3bbdb64d93f
Estchho <33856056+Estchho@users.noreply.github.com>
2019-07-18 17:54:22 +1000

    18,00

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     //initialize swiper when document ready
     var mySwiper = new Swiper ('.swiper-container', {
@@ -10,8 +9,17 @@ $(document).ready(function () {
         draggable: true,
       },
       loop: true
-    })
+    });
+
+    $(function() {
+      $("#phone").mask("+7(999)999-99-99", {
+        placeholder: "+7(___)___-__-__",
+      });
+    });
+
     $('#calendar').clndr();
   });
-
+ 
+    
+  
   
