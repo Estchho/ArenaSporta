32e51ed8aaa719550a0a3e3105a49da4d9ebdfe0
Estchho <33856056+Estchho@users.noreply.github.com>
2019-07-22 11:03:04 +1000

    header complete

--- js/script.js
+++ js/script.js
@@ -13,15 +13,16 @@ $(document).ready(function () {
     
     $(window).scroll(function() {
       if ($(this).scrollTop() > 1){
-      $('header').addClass("sticky");
+      $('header').addClass('sticky');
       $('.header__nav__link').addClass("header__nav__link-scroll");
       $('.header__logo').html('<img src="img/Header_logo-scroll.svg" alt=""></img>');
-      
+      $('.header__tool').html('<div class="header__tool_cal" id="calendar"><img src="img/Header_cal-scroll.svg" alt=""></div><a href="#" class="header__tool_phone"><img src="img/Header_mail.svg" alt=""></a>')
       }
       else{
-      $('header').removeClass("sticky");
-      $('.header__nav__link').removeClass("header__nav__link-scroll")
+      $('header').removeClass('sticky');
+      $('.header__nav__link').removeClass('header__nav__link-scroll')
       $('.header__logo').html('<img src="img/Header_logo.svg" alt="">');
+      $('.header__tool').html('<div class="header__tool_cal" id="calendar"><img src="img/Header_cal.svg" alt=""></div><a href="#" class="header__tool_phone"><img src="img/Header_phone.svg" alt=""></a>')
       }
       });
 
