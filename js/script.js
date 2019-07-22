b6e52dadf2506fe6742bd31c5dab2ed8e29b948f
Estchho <33856056+Estchho@users.noreply.github.com>
2019-07-22 11:48:37 +1000

    maito tel

--- js/script.js
+++ js/script.js
@@ -16,13 +16,13 @@ $(document).ready(function () {
       $('header').addClass('sticky');
       $('.header__nav__link').addClass("header__nav__link-scroll");
       $('.header__logo').html('<img src="img/Header_logo-scroll.svg" alt=""></img>');
-      $('.header__tool').html('<div class="header__tool_cal" id="calendar"><img src="img/Header_cal-scroll.svg" alt=""></div><a href="#" class="header__tool_phone"><img src="img/Header_mail.svg" alt=""></a>')
+      $('.header__tool').html('<div class="header__tool_cal" id="calendar"><img src="img/Header_cal-scroll.svg" alt=""></div><a href="mailto:name@mail.com" class="header__tool_phone"><img src="img/Header_mail.svg" alt=""></a>')
       }
       else{
       $('header').removeClass('sticky');
       $('.header__nav__link').removeClass('header__nav__link-scroll')
       $('.header__logo').html('<img src="img/Header_logo.svg" alt="">');
-      $('.header__tool').html('<div class="header__tool_cal" id="calendar"><img src="img/Header_cal.svg" alt=""></div><a href="#" class="header__tool_phone"><img src="img/Header_phone.svg" alt=""></a>')
+      $('.header__tool').html('<div class="header__tool_cal" id="calendar"><img src="img/Header_cal.svg" alt=""></div><a href="tel:+79510000000" class="header__tool_phone"><img src="img/Header_phone.svg" alt=""></a>')
       }
       });
 
