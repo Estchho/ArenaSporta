b47b56da5a35b7032715588fc704d8e832fcaa52
vkrms <april504@yandex.ru>
2019-07-31 12:44:13 +1000

    pre-alpha-release

--- js/script.js
+++ js/script.js
@@ -29,16 +29,16 @@ $(document).ready(function () {
 
     $(window).scroll(function() {
       if ($(this).scrollTop() > 1){
-      $('header').addClass('sticky');
-      $('.header__nav__link').addClass("header__nav__link-scroll");
-      $('.header__logo').html('<img src="img/Header_logo-scroll.svg" alt=""></img>');
-      $('.header__tool').html('<div class="header__tool_cal" id="calendar"><img src="img/Header_cal-scroll.svg" alt=""></div><a href="tel:+79510000000" class="header__tool_phone"><img src="img/Header_phone-scroll.svg" alt=""></a>')
+        $('header').addClass('sticky');
+        $('.header__nav__link').addClass("header__nav__link-scroll");
+        // $('.header__logo').html('<img src="img/Header_logo-scroll.svg" alt=""></img>');
+        // $('.header__tool').html('<div class="header__tool_cal" id="calendar"><img src="img/Header_cal-scroll.svg" alt=""></div><a href="tel:+79510000000" class="header__tool_phone"><img src="img/Header_phone-scroll.svg" alt=""></a>')
       }
-      else{
-      $('header').removeClass('sticky');
-      $('.header__nav__link').removeClass('header__nav__link-scroll')
-      $('.header__logo').html('<img src="img/Header_logo.svg" alt="">');
-      $('.header__tool').html('<div class="header__tool_cal" id="calendar"><img src="img/Header_cal.svg" alt=""></div><a href="tel:+79510000000" class="header__tool_phone"><img src="img/Header_phone.svg" alt=""></a>')
+      else {
+        $('header').removeClass('sticky');
+        $('.header__nav__link').removeClass('header__nav__link-scroll')
+        // $('.header__logo').html('<img src="img/Header_logo.svg" alt="">');
+        // $('.header__tool').html('<div class="header__tool_cal" id="calendar"><img src="img/Header_cal.svg" alt=""></div><a href="tel:+79510000000" class="header__tool_phone"><img src="img/Header_phone.svg" alt=""></a>')
       }
     });
 
@@ -57,7 +57,7 @@ $(document).ready(function () {
 
   // init PLYR
   var player = new Plyr('#hero-player', {
-      // ratio: '16:7',
+      ratio: '16:7',
       poster: '/img/poster.jpg'
   });
 
