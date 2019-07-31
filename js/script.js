efa50a3e0b5da0654f374ebbf475a2a80922c8b8
vkrms <april504@yandex.ru>
2019-07-31 15:59:14 +1000

    wip ))

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ $(document).ready(function () {
       },
     });
 
-
     //initialize swiper when document ready
     var mySwiper = new Swiper ('.swiper-team', {
       // Optional parameters
@@ -27,18 +26,21 @@ $(document).ready(function () {
       loop: true
     });
 
+    // init about swiper
+    var aboutSwiper = new Swiper ('.about__slider', {
+      // Optional parameters
+      slidesPerView: 1,
+      loop: true
+    });
+
     $(window).scroll(function() {
       if ($(this).scrollTop() > 1){
         $('header').addClass('sticky');
         $('.header__nav__link').addClass("header__nav__link-scroll");
-        // $('.header__logo').html('<img src="img/Header_logo-scroll.svg" alt=""></img>');
-        // $('.header__tool').html('<div class="header__tool_cal" id="calendar"><img src="img/Header_cal-scroll.svg" alt=""></div><a href="tel:+79510000000" class="header__tool_phone"><img src="img/Header_phone-scroll.svg" alt=""></a>')
       }
       else {
         $('header').removeClass('sticky');
         $('.header__nav__link').removeClass('header__nav__link-scroll')
-        // $('.header__logo').html('<img src="img/Header_logo.svg" alt="">');
-        // $('.header__tool').html('<div class="header__tool_cal" id="calendar"><img src="img/Header_cal.svg" alt=""></div><a href="tel:+79510000000" class="header__tool_phone"><img src="img/Header_phone.svg" alt=""></a>')
       }
     });
 
@@ -57,7 +59,7 @@ $(document).ready(function () {
 
   // init PLYR
   var player = new Plyr('#hero-player', {
-      ratio: '16:7',
+      // ratio: '16:7',
       poster: '/img/poster.jpg'
   });
 
@@ -80,3 +82,6 @@ $(document).ready(function () {
     player.togglePlay();
     $('.hero__slider-wrap').toggleClass('--hidden');
   });
+
+  // init tabby tabs
+  var tabs = new Tabby('[data-tabs]');
