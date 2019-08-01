65ccb2bc7d55864bb14d100d87d8fc9368cc5a0a
Estchho <33856056+Estchho@users.noreply.github.com>
2019-08-01 17:48:24 +1000

    input hover

--- js/script.js
+++ js/script.js
@@ -90,6 +90,12 @@ $(document).ready(function () {
   // init tabby tabs
   var tabs = new Tabby('[data-tabs]');
 
+  // modal mask
+  var phoneMask = IMask(
+    document.getElementById('modal__phone'), {
+      mask: '+{7}(000)000-00-00'
+    });
+    
   // preloader
   $('.above-fold').on('load error', function(e){
     $('#preloader').removeClass('--visible');
